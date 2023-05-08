c232af3633f5115607dfac3a0c42dcd3dc7ed1db
time9v <lol.time9@gmail.com>
2023-05-08 13:06:31 +0300

    Исправил вспомогательную функцию, оставил пустую строку между импортом и константой

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,16 +1,13 @@
 import random
+
 GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(question):
+    return question % 2 == 0
 
 
 def get_game():
     question = random.randrange(1, 100)
-    correct_answer = even_logic(question)
+    correct_answer = 'yes' if is_even(question) else 'no'
     return question, correct_answer
-
-
-def even_logic(question):
-    if question % 2 == 0:
-        answer = 'yes'
-    else:
-        answer = 'no'
-    return answer
