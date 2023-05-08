e46709dd5e31edd2f057ec63c874213d9162cd92
time9v <lol.time9@gmail.com>
2023-05-08 13:10:05 +0300

    Изменил и переименовал функцию, оставил пустую строку между константой и импортом

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,21 +1,21 @@
 import random
+
 GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def get_game():
-    num = random.randrange(1, 100)
-    question = num
-    correct_answer = prime_logic(question)
-    return question, correct_answer
-
-
-def prime_logic(question):
+def is_prime(question):
     k = 0
     for i in range(2, question // 2 + 1):
         if question % i == 0:
             k += 1
     if k <= 0:
-        answer = 'yes'
+        return True
     else:
-        answer = 'no'
-    return answer
+        return False
+
+
+def get_game():
+    num = random.randrange(1, 100)
+    question = num
+    correct_answer = 'yes' if is_prime(question) else 'no'
+    return question, correct_answer
